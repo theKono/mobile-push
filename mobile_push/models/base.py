75d58fbfb21767443747c0bcf9f5bbc364d7461f
Yu Liang <yu.liang@thekono.com>
2016-01-04 17:05:41 +0800

    Create Topic model

---
+++ mobile_push/models/base.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# standard library imports
+
+# third party related imports
+from sqlalchemy.ext.declarative import declarative_base
+
+# local library imports
+
+
+Base = declarative_base()
