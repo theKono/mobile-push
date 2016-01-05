ae59b6c3c42dfa9dc7b7847bc8c3eae7f26eb39f
Yu Liang <yu.liang@thekono.com>
2016-01-05 18:29:31 +0800

    Create Subscription model

--- mobile_push/db.py
+++ mobile_push/db.py
@@ -13,6 +13,7 @@ from mobile_push.models.base import Base
 # We should import all model before we can create all tables
 from mobile_push.models.apns_token import ApnsToken
 from mobile_push.models.gcm_token import GcmToken
+from mobile_push.models.subscription import Subscription
 from mobile_push.models.topic import Topic
 
 
