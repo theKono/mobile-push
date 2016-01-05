4c20a9a5d090d89478296ed0191da15d07ad3644
Yu Liang <yu.liang@thekono.com>
2016-01-05 19:21:18 +0800

    Write SubscribeTopicActor#run spec

--- tests/actors/test_subscribe_topic.py
+++ tests/actors/test_subscribe_topic.py
@@ -23,28 +23,26 @@ class TestFindTokenEndpointArns(BaseTestCase):
     def test_when_there_exists_an_apns_token(self):
 
         session = Session()
-        at = ApnsToken(
+        session.add(ApnsToken(
             token='qq',
             application_arn='app-arn',
             endpoint_arn='arn'
-        )
-        session.add(at)
+        ))
         session.commit()
 
-        self.assertEqual(self.actor.find_token_endpoint_arns('qq'), [at])
+        self.assertEqual(self.actor.find_token_endpoint_arns('qq'), ['arn'])
 
     def test_when_there_exists_a_gcm_token(self):
 
         session = Session()
-        gt = GcmToken(
+        session.add(GcmToken(
             token='qq',
             application_arn='app-arn',
             endpoint_arn='arn'
-        )
-        session.add(gt)
+        ))
         session.commit()
 
-        self.assertEqual(self.actor.find_token_endpoint_arns('qq'), [gt])
+        self.assertEqual(self.actor.find_token_endpoint_arns('qq'), ['arn'])
 
     def test_when_there_is_no_such_token(self):
 
@@ -60,11 +58,10 @@ class TestFindTopicArn(BaseTestCase):
     def test_when_ther_exists_a_topic(self):
 
         session = Session()
-        t = Topic(name='qq', arn='arn')
-        session.add(t)
+        session.add(Topic(name='qq', arn='arn'))
         session.commit()
 
-        self.assertEqual(self.actor.find_topic_arn('qq'), t)
+        self.assertEqual(self.actor.find_topic_arn('qq'), 'arn')
 
     def test_when_there_is_no_such_topic(self):
 
@@ -119,3 +116,66 @@ class TestSaveSubscription(BaseTestCase):
         self.assertEqual(s.topic_arn, 't-arn')
         self.assertEqual(s.endpoint_arn, 'e-arn')
         self.assertEqual(s.subscription_arn, 's-arn')
+
+
+class TestRun(BaseTestCase):
+
+    def setUp(self):
+
+        self.actor = SubscribeTopicActor()
+        self.actor.find_token_endpoint_arns = MagicMock()
+        self.actor.find_topic_arn = MagicMock()
+        self.actor.call_sns_api = MagicMock()
+        self.actor.get_arn_from_response = MagicMock()
+        self.actor.save_subscription = MagicMock()
+
+    def test_when_token_is_not_present(self):
+
+        message = {'args': {}}
+
+        self.actor.run(message)
+        self.assertFalse(self.actor.call_sns_api.called)
+
+    def test_when_token_endpoint_arn_is_not_found(self):
+
+        message = {'args': {'token': 'qq'}}
+
+        self.actor.find_token_endpoint_arns.return_value = []
+        self.actor.run(message)
+        self.assertFalse(self.actor.call_sns_api.called)
+
+    def test_when_topic_is_not_present(self):
+
+        message = {'args': {'token': 'qq'}}
+
+        self.actor.find_token_endpoint_arns.return_value = [True]
+        self.actor.run(message)
+        self.assertFalse(self.actor.call_sns_api.called)
+
+    def test_when_topic_is_not_found(self):
+
+        message = {'args': {'token': 'qq', 'topic': 'gg'}}
+
+        self.actor.find_token_endpoint_arns.return_value = [True]
+        self.actor.find_topic_arn.return_value = None
+        self.actor.run(message)
+        self.assertFalse(self.actor.call_sns_api.called)
+
+    def test_when_everything_is_ok(self):
+
+        message = {'args': {'token': 'qq', 'topic': 'gg'}}
+
+        self.actor.find_token_endpoint_arns.return_value = ['endpoint-arn']
+        self.actor.find_topic_arn.return_value = 'topic-arn'
+        self.actor.call_sns_api.return_value = resp = MagicMock()
+        self.actor.get_arn_from_response.return_value = 'subscription-arn'
+
+        self.actor.run(message)
+
+        self.actor.call_sns_api.assert_called_with('topic-arn', 'endpoint-arn')
+        self.actor.get_arn_from_response.assert_called_with(resp)
+        self.actor.save_subscription.assert_called_with(
+            'topic-arn',
+            'endpoint-arn',
+            'subscription-arn'
+        )
