81c00b7b3fb5c7c29a613ef32725d08c9b516eca
Yu Liang <yu.liang@thekono.com>
2016-01-07 01:14:00 +0800

    Write PublishToTopicActor#call_sns_api spec

--- tests/actors/test_publish_to_topic.py
+++ tests/actors/test_publish_to_topic.py
@@ -3,6 +3,7 @@
 # standard library imports
 
 # third party related imports
+from mock import MagicMock
 
 # local library imports
 from mobile_push.actors.publish_to_topic import PublishToTopicActor
@@ -24,3 +25,31 @@ class TestFindTopicArn(BaseTestCase):
     def test_when_there_is_no_such_topic(self):
 
         self.assertIsNone(self.actor.find_topic_arn('qq'))
+
+
+class TestCallSnsApi(BaseTestCase):
+
+    def setUp(self):
+
+        self.actor = PublishToTopicActor()
+        self.actor.sns_conn = MagicMock()
+
+    def test(self):
+
+        self.actor.call_sns_api('topic-arn', 'message')
+        self.actor.sns_conn.publish.assert_called_with(
+            topic='topic-arn',
+            message='message',
+            message_structure='json'
+        )
+
+
+class TestRun(BaseTestCase):
+
+    def setUp(self):
+
+        self.actor = PublishToTopicActor()
+
+    def test_when_topic_is_not_present(self):
+
+        self.actor
